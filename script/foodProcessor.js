f76b0a09dd650053024a8a8150f026ac810bfc17
Linnéa Toth <linnea@liroit.com>
2025-03-18 10:22:50 +0100

    code cleaning

--- script/foodProcessor.js
+++ script/foodProcessor.js
@@ -1,8 +1,9 @@
 "use strict";
 
-//This script deals with putting menu item on the site. The output depends on whether the active page is Lunch or Menu.
+//The script on this page deals with putting menu item on the site. The output depends on whether the active page is Lunch or Menu.
 
 document.addEventListener("DOMContentLoaded", async () => {
+
     const body = document.querySelector("body");
     const dishes = []; //will be loaded with instances of MenuItem's, representing the menu
     let cart = []; //will be loaded with items when added by user
@@ -17,16 +18,15 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     }
 
-    //Returns what day of the week it is today
+    //Helper function that returns what day of the week it is today
     const weekdayName = function () {
         const date = new Date();
         const weekdayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         let weekday = weekdayNames[date.getDay()];
-        console.log("Today is " + weekday);
         return weekday;
     }
 
-    //Function building he scopping cart
+    //Builds the cart
     const buildCart = function (arrayCart) {
         const shoppingSection = document.querySelector("#shopping-cart");
         shoppingSection.innerHTML = "<h3>Your dishes</h3>"; //empties the section, so the cart isn't added to last rendering of it
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         let lastDishPrice;
         let sumDishes = 0;
 
-        cart.sort(function (a, b) { //algorithm found on w3schools array sort doc
+        cart.sort(function (a, b) { //algorithm found on w3schools array sort doc. Places identical items next to each other in the array. 
             let x = a.dishName;
             let y = b.dishName;
             if (x < y) { return -1; }
@@ -46,8 +46,10 @@ document.addEventListener("DOMContentLoaded", async () => {
             if (x === y) { return 0; }
         });
 
+        //Going through each of the items in the cart
         for (let j = 0; j < cart.length; j++) {
             if (lastDish === cart[j]) {
+                //If there are double listings, the amount of the existing item will increase instead of the page listing them twice
                 quantity++;
                 lastDishQuantityText.innerText = `Amount: ${quantity}`;
                 lastDishPriceText.innerText = `Price: ${quantity * lastDishPrice} sek (${lastDishPrice} sek per serving)`;
@@ -68,21 +70,27 @@ document.addEventListener("DOMContentLoaded", async () => {
                 priceText.innerText = `Price: ${lastDishPrice}`;
                 dishArticle.appendChild(dishHeading);
 
+                //Adds the trash can icon
                 const removeItemH4 = document.createElement("h4");
                 const removeItem = document.createElement("i");
                 removeItem.classList.add("fa-solid", "fa-trash-can");
 
+                //Puts the elements together
                 removeItemH4.appendChild(removeItem)
                 dishArticle.appendChild(removeItemH4);
                 dishArticle.appendChild(quantityText);
                 dishArticle.appendChild(priceText);
                 shoppingSection.appendChild(dishArticle);
 
+                //Stores the current dish for comparison in the next turn of the loop
                 lastDish = arrayCart[j];
+                //Current sum of cost
                 sumDishes += arrayCart[j].dishPrice;
+                //Stores price elements for the next round in case it's a double listed item and needs to be changed 
                 lastDishPriceText = priceText;
                 lastDishQuantityText = quantityText;
 
+                //Adds eventlisteners for each cart item. It will return an array with everything that is not the item that is being removed. Then the cart will be rebuilt. 
                 removeItem.addEventListener("click", () => {
                     cart = arrayCart.filter((cartItem) => {
                         return cartItem.dishName !== dishName;
@@ -98,14 +106,11 @@ document.addEventListener("DOMContentLoaded", async () => {
         shoppingSection.appendChild(sumHeading);
     }
 
-
-
     async function getFoodData() { //function that gets data with dishes from json-file
         try {
             const response = await fetch("./script/dishes.json");
             const dishesData = await response.json();
-            return dishesData;
-
+            return dishesData; //returns an array with all of the dishes
         } catch (error) {
             console.log(error.message);
         }
@@ -115,20 +120,20 @@ document.addEventListener("DOMContentLoaded", async () => {
         const dishesData = await getFoodData();
         let dishCounter = 0; //used for reaching the correct index in the dishes array
 
-        for (let dishData of dishesData) {
-            dishes[dishCounter] = new MenuItem(...dishData); // ... is used for spreading the array into individual arguments, separated by commas!  
-            dishCounter++;
+        for (let [i, dishData] of dishesData.entries()) { //adding .entries() is a workaround to be able to get the index of the array items when using for...of
+            dishes[i] = new MenuItem(...dishData);
         }
     }
 
     //Function putting the lunch page content together and in place
-    const buildLunchSite = async function () {
+    const buildLunchSite = function () {
         const date = new Date();
 
         if (body.id != "lunch") {
             return;
         }
         try {
+            //declaring and modifyring DOM elements
             const lunchDetailsBtn = document.querySelector("#detailed-lunch-info-button")
             const detailedLunchInfo = document.querySelector("#detailed-lunch-info");
             const dishContainer = document.querySelector(".lunch-dish");
@@ -138,13 +143,14 @@ document.addEventListener("DOMContentLoaded", async () => {
             const lunchDishPrice = document.createElement("p");
             lunchDishPrice.classList.add("lunch-dish-price");
             const weekdayHeading = document.querySelector("#weekday");
-
             weekdayHeading.innerText = weekdayName() + " deal:";
 
+            //Button hiding and showing lunch details, using the helper function
             lunchDetailsBtn.addEventListener("click", () => {
                 toggleDisplay(detailedLunchInfo)
             });
 
+            //Types out the lunch offer if today is a weekday
             if (date.getDay() > 0 && date.getDay() < 6) {
                 for (let dish of dishes) {
                     if (dish.isLunchOffer) {
@@ -157,6 +163,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                     }
                 }
             } else {
+                //Wishes you a happy weekend if today is a Saturday or Sunday
                 weekdayHeading.innerText = "Happy " + weekdayName() + "!  Welcome back for lunch deals offered from Monday through Friday!";
             }
         }
@@ -166,11 +173,12 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     //Function putting the menu page content together and in place
-    const buildMenuSite = async function () {
+    const buildMenuSite = function () {
         if (body.id != "menu") {
-            return;
+            return; //if it's not the menu page, the rest of the code will not run
         }
         try {
+            //declaring and modifyring DOM elements
             const starterSection = document.querySelector("#starter-section");
             const mainSection = document.querySelector("#main-section");
             const dessertSection = document.querySelector("#dessert-section");
@@ -214,7 +222,6 @@ document.addEventListener("DOMContentLoaded", async () => {
             });
 
             //Looping through the dishes, putting them on the site
-            let dishCounter = 0;
             for (let dish of dishes) {
                 const dishArticle = document.createElement("article");
                 dishArticle.classList.add("dish-article");
@@ -275,8 +282,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
     await createDishes(); //Needed everywhere there is a mention of menu items :)
-    await buildLunchSite(); //Will only run through the code if on lunch page
-    await buildMenuSite(); //Will only run through the code if on menu page
+    buildLunchSite(); //Will only run through the code if on lunch page
+    buildMenuSite(); //Will only run through the code if on menu page
 
 
 })
--- script/formValidation.js
+++ script/formValidation.js
@@ -1,8 +1,8 @@
+"use strict";
+
 //This script will validate the input from the form 
 
-const submitBtn = document.querySelector("#submit-btn");
-
-let ul = null;
+let ul = null; //List with errors is empty from the beginning
 
 const validateForm = function () {
     let allOk = true;
@@ -15,13 +15,12 @@ const validateForm = function () {
     const formFreeText = document.forms["res-inq-form"]["free_text"].value;
     const formPhone = document.forms["res-inq-form"]["phone"].value.trim();
 
+    //Tried to create a formatting criteria for each input, using regex
     const regExMail = /^[\w.]{1,20}@[\w-]{1,20}\.[a-zA-Z]{2,8}$/;
     const regExPhone = /^[\+]?[0-9]{0,4}?[0-9]{0,4}[\-]?[0-9]{5,10}$/;
     const regExName = /^[a-zA-ZÀ-ÿ]+[a-zA-ZÀ-ÿ\s'-]{0,44}$/;
 
-    console.log(regExMail);
-    console.log(formEmail.match(regExMail))
-
+    //Checking each field, adding the missing info to the list if something is off
     if (!formFreeText || formFreeText.length < 10) {
         const li = document.createElement("li");
         li.innerText = "A message for us";
@@ -50,6 +49,7 @@ const validateForm = function () {
         allOk = false;
     }
 
+    //If all wasn't okay in the check, the info will be added to a list below the form
     if (!allOk) {
         errorBox.appendChild(ul);
     }
@@ -57,15 +57,17 @@ const validateForm = function () {
     return allOk;
 }
 
-submitBtn.addEventListener("click", async function (event) {
+const submitBtn = document.querySelector("#submit-btn");
+
+submitBtn.addEventListener("click", (event) => {
     const errorBox = document.querySelector("#error-box");
 
-    if (ul) { ul.remove(); };
+    if (ul) {
+        ul.remove();
+    };
 
     if (!validateForm()) {
         event.preventDefault(); //prevents default submission of form
-        console.log("Submission prevented");
-
     } else {
         confirm("Thank you for reaching out to us! You will hear from us soon!");
     };
